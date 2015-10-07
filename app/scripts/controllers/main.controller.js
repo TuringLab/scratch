85580e963d2a6515bd0ba24144dfd5613e9c823d
Henry Miskin <henry.miskin@talktalk.net>
2015-10-07 19:51:03 +0100

    Added cross domain message posting

--- app/scripts/controllers/main.controller.js
+++ app/scripts/controllers/main.controller.js
@@ -2,5 +2,16 @@
 
 angular.module('turingScratch').controller('MainController',['$scope',function($scope){
 
-    $scope.test = "HELLO WORLD"
+  $scope.test = "HELLO WORLD";
+  $scope.messages = [];
+  $scope.$root.$on('$messageIncoming', function (event, data){
+    $scope.messages.push(angular.fromJson(data));
+  });
+  $scope.sendMessage = function (){
+    $scope.$emit(
+      '$messageOutgoing',
+      angular.toJson({"response":"hi"})
+    );
+  };
+
 }]);
